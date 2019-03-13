374f50018fc87d5fb390e5df0e00d79d5436c656
AToMiXhawK <githinmanesh@gmail.com>
2019-03-13 17:53:58 +0530

    Program to find largest of 3 nos

---
+++ 2~largest_of_3_nos.py
@@ -0,0 +1,13 @@
+a=input ("Enter value of a: ")
+b=input ("Enter value of b: ")
+c=input ("Enter value of c: ")
+if a>b:
+	if a>c:
+		print "The Largest number is",a
+	else:
+		print "The Largest number is",c
+else:
+	if b>c:
+		print "The Largest number is",b
+	else:
+		print "The Largest number is",c
